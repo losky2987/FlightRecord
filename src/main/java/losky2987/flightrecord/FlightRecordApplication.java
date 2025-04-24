2a8e8372f58d3f3c578a31401eeb793ba29ab1b3
losky2987 <losky2987@gmail.com>
2025-04-24 06:41:21 +0200

    init push

---
+++ src/main/java/losky2987/flightrecord/FlightRecordApplication.java
@@ -0,0 +1,13 @@
+package losky2987.flightrecord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightRecordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightRecordApplication.class, args);
+	}
+
+}
---
+++ src/test/java/losky2987/flightrecord/FlightRecordApplicationTests.java
@@ -0,0 +1,13 @@
+package losky2987.flightrecord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightRecordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
